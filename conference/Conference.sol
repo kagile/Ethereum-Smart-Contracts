8988bbb9baa638da819b38019d167569be1f924b
krupal <krupal.shah@agileinfoways.com>
2018-07-25 13:46:28 +0530

    Update Conference.sol

--- conference/Conference.sol
+++ conference/Conference.sol
@@ -8,8 +8,8 @@ import "./SafeMath.sol";
  * Also, if someone asks for a refund, he/she can request cancellation to the organizer and only organizer can provide the refund.
  */
 contract Conference {
-    
-    using SafeMath for uint256;
+	
+	using SafeMath for uint256;
 
 	address  organizer;
 	mapping (address => uint)  registrantsPaid; //mapping of attendee and his amount sent
@@ -19,25 +19,25 @@ contract Conference {
 
 	event Deposit(address _from, uint _amount); //when the ticket is bought, this event will be fired
 	event Refund(address _to, uint _amount); //when the refund is given, this event will be fired
-	
+
 	/**
 	 * this modifier will restrict opertaions to be done only by organizer.
 	 */
-    modifier restricted() {
-         require (msg.sender == organizer);
-         _;
-    }
-  
-	 constructor() public {
+	modifier restricted() {
+	 require (msg.sender == organizer);
+	 _;
+	}
+
+	constructor() public {
 		organizer = msg.sender;		
 		quota = 100; //initial quota is 100. it can be changed using external function.
 		numRegistrants = 0;
 		ticketPrice = 0.1 ether; //ticet price is 0.1 ether and it can not be changed once the contract is published
 	}
 
-    /**
-     * function for buying tickets. it will check the amount sent by sender, quota and whether user has already bought the ticket or not.
-     */
+	/**
+	* function for buying tickets. it will check the amount sent by sender, quota and whether user has already bought the ticket or not.
+	*/
 	function buyTicket() external payable {
 	    require(msg.value == ticketPrice,"Received amount is not equal to ticket price");
 		require (numRegistrants <= quota,"Quota full. No tickets left"); 
@@ -47,20 +47,20 @@ contract Conference {
 		emit Deposit(msg.sender, msg.value);
 	}
 
-    function getTicketPrice() view external returns(uint)   {
-        return ticketPrice;
-    }
+	function getTicketPrice() view external returns(uint)   {
+	return ticketPrice;
+	}
 
-    
+
 	function changeQuota(uint _newquota) external restricted {
 		quota = _newquota;
 	}
-	
-    /**
-     * function for providing refund. It can only be done by the organizer.
-     * It will also check the ownership of ticket and balance of organizer.
-     * After that, it will transfer the refund amount to the attendee who has requested for refund.
-     */
+
+	/**
+	* function for providing refund. It can only be done by the organizer.
+	* It will also check the ownership of ticket and balance of organizer.
+	* After that, it will transfer the refund amount to the attendee who has requested for refund.
+	*/
 	function refundTicket(address _recipient) external payable restricted {
 		require (registrantsPaid[_recipient] == ticketPrice, "You have not paid anything");
 		address myAddress = this;
@@ -70,7 +70,7 @@ contract Conference {
 		registrantsPaid[_recipient] = 0;
 		numRegistrants.sub(1);
 	}
-	
+
 	function destroy() external restricted {
 		selfdestruct(organizer); //contract can be destroyed so that the organizer gets all the amount gathered.
 	}
